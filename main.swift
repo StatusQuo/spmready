5cd94583de7a36ffb8263b75695707bae48e9654
StatusQuo <bastihumann@googlemail.com>
2019-09-18 08:08:31 +0200

    refactor networking and regular expressions

--- main.swift
+++ main.swift
@@ -25,6 +25,22 @@ extension Library {
     }
 }
 
+enum Searcher: String {
+   case pod = "pod [\"']([A-Za-z0–9-]*)[\"']"
+   case podRepoUrl = "(((https?):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\.&]*))\">GitHub Repo</a>"
+}
+
+extension String {
+    func match(_ search: Searcher) -> String? {
+        let regex = NSRegularExpression(search.rawValue)
+        if  let match = regex.matches(self), let matchRange = Range(match.range(at: 1), in: self) {
+            let result = self[matchRange]
+            return String(result)
+        }
+        return nil
+    }
+}
+
 extension NSRegularExpression {
     convenience init(_ pattern: String) {
         do {
@@ -42,23 +58,14 @@ extension NSRegularExpression {
     }
 }
 
-func findPodName(_ input: String) -> String? {
-    let regex = NSRegularExpression("pod [\"']([A-Za-z0–9-]*)[\"']")
-    if  let match = regex.matches(input), let podNameRange = Range(match.range(at: 1), in: input) {
-        let podName = input[podNameRange]
-        return String(podName)
-    }
-    return nil
-}
-
 func fetchPods(_ path: String) -> [Library]? {
     var pods:[Library] = []
     do {
         // Get the contents
         let contents = try String(contentsOfFile: path, encoding: .utf8)
-        let lines = contents.split(separator: "\n")
+        let lines = contents.split(separator: "\n").map { String($0) }
         for line in lines {
-            if let podName = findPodName(String(line)) {
+            if let podName = line.match(.pod) {
                 pods.append(Library(name: podName))
             }
         }
@@ -74,43 +81,57 @@ func fetchPods(_ path: String) -> [Library]? {
 
 func fetchUrl(pod: String) -> String {
 
-    let semaphore = DispatchSemaphore(value: 0)
-
     let path = "https://cocoapods.org/pods/\(pod)"
 
-    var result = ""
-
-    guard let url = URL(string: path) else {
+    guard let data = get(url: path).take()?.data,
+        let contents = String(data: data, encoding: .utf8) else {
         return ""
     }
 
-    let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
-        if let data = data {
-            result = String(data: data, encoding: .utf8) ?? ""
-        }
+    return contents
 
-        semaphore.signal()
-    }
-
-    task.resume()
-
-    _ = semaphore.wait(timeout: .distantFuture)
-    return result
 }
 
 
-func isSpmReady(repo: String) -> Bool {
-    let spmUrl = repo.replacingOccurrences(of: ".git", with: "") + "/blob/master/Package.swift"
+func hasSwiftPackageFile(repoUrl: String) -> Bool {
+    let spmUrl = repoUrl.replacingOccurrences(of: ".git", with: "") + "/blob/master/Package.swift"
 
-    var result = false
+    if let code = get(url: spmUrl).take()?.code, code == 200 {
+        return true
+    }
+
+    return false
+}
+
+struct HttpResult {
+    let code: Int
+    let data: Data?
+}
+
+enum HttpError: Error {
+    case error
+}
+
+extension Result {
+    func take() -> Success? {
+        switch self {
+        case .success(let data):
+            return data
+        case .failure:
+            return nil
+        }
+    }
+}
+
+func get(url: String) -> Result<HttpResult, HttpError> {
+
+    var result: Result<HttpResult, HttpError> = .failure(HttpError.error)
     let semaphore = DispatchSemaphore(value: 0)
-    let url = URL(string: spmUrl)!
+    let url = URL(string: url)!
 
     let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
         if let httpResponse = response as? HTTPURLResponse {
-            if httpResponse.statusCode == 200 {
-                result = true
-            }
+            result = .success(HttpResult(code: httpResponse.statusCode, data: data))
         }
 
         semaphore.signal()
@@ -123,20 +144,8 @@ func isSpmReady(repo: String) -> Bool {
 }
 
 func fetchRepoOnline(podName: String) -> String? {
-    let page = fetchUrl(pod: podName)
-
-    let regex = NSRegularExpression("(((https?):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\.&]*))\">GitHub Repo</a>")
-
-    if  let match = regex.matches(String(page)) {
-        if let podNameRange = Range(match.range(at: 1), in: page) {
-            let repo = page[podNameRange]
-            return String(repo)
-        }
-    }
-
-    return nil
+    return fetchUrl(pod: podName).match(.podRepoUrl)
 }
-
 
 let path: String
 
@@ -162,7 +171,7 @@ for pod in pods {
         pod.repo = url
     }
 
-    if let repo = pod.repo, isSpmReady(repo: repo) {
+    if let repo = pod.repo, hasSwiftPackageFile(repoUrl: repo) {
         pod.spmready = true
     }
 
